847dd051dd4221f6015540b3eca66e1b2315c71d
T. Matsumori <tmatsumor@users.noreply.github.com>
2023-06-18 13:26:22 +0900

    Create http_requests.php

---
+++ http_requests.php
@@ -0,0 +1,35 @@
+<?php
+class HttpRequests
+{
+    public static function get($url, $param, $header=null){
+        $ch=curl_init($url.'?'.http_build_query($param).'&'.uniqid());
+        return self::no_cache($ch);
+    }
+
+    public static function post($url, $param, $header=null){
+        $ch=curl_init($url.'?'.uniqid());
+        curl_setopt($ch,CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($param));
+        return self::no_cache($ch);
+    }
+
+    private static function no_cache($ch, $header=null){
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array_merge(
+            ['Cache-Control: private, no-store, no-cache, must-revalidate', 'Pragma: no-cache'],
+            (is_null($header))? [] : $header));
+        $body  = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        $errno = curl_errno($ch);
+        $error = curl_error($ch);
+        curl_close($ch);
+        if (CURLE_OK !== $errno) {
+            throw new RuntimeException($error, $errno);
+        }
+        return [$body, $info];
+    }
+}
+?>
